f8e51c5794f5678de2f436d492cb24ca841ba14a
FilipeJohasson <filipejohansson7@gmail.com>
2021-09-06 17:02:47 -0300

    getting user dir

--- src/main/java/action/XMLParse.java
+++ src/main/java/action/XMLParse.java
@@ -11,22 +11,19 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-import java.util.Scanner;
 import javax.xml.XMLConstants;
 
 public class XMLParse {
-	private static final String FILENAME = "C://file.xml";
+	private static final String USER_DIR = System.getProperty("user.dir");
+	private static final String FILENAME = USER_DIR + "/file.xml";
 
 	public String getRandomWord() {
+		System.out.println("a: " + FILENAME);
 		// Instantiate the Factory
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 
 		try {
-			// optional, but recommended
 			// process XML securely, avoid attacks like XML External Entities (XXE)
 			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
 
@@ -34,12 +31,8 @@ public class XMLParse {
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			Document doc = db.parse(new File(FILENAME));
 
-			// optional, but recommended
 			// http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
 			doc.getDocumentElement().normalize();
-
-			System.out.println("Root Element: " + doc.getDocumentElement().getNodeName());
-			System.out.println("------");
 
 			// get <word>
 			NodeList list = doc.getElementsByTagName("word");
@@ -47,7 +40,6 @@ public class XMLParse {
 			Random random = new Random();
 			int r = random.nextInt(list.getLength());
 
-			// System.out.println("R: " + r);
 			Node node = list.item(r);
 
 			if (node.getNodeType() == Node.ELEMENT_NODE) {
